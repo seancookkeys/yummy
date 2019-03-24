cc0b4c4a14c6fe00f7bad0f0ac16fdf404beb169
Sean Cook <you@sean.bernard.cook@gmail.com>
2019-03-24 23:25:33 +0000

    added destroy security

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,5 +1,5 @@
 class PlacesController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :create, :edit, :update]
+  before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
 
 
   def index
@@ -28,12 +28,18 @@ class PlacesController < ApplicationController
 
   def update
     @place = Place.find(params[:id])
+    if @place.user != current_user 
+      return render plain: 'Not Allowed', status: :forbidden
+    end
     @place.update_attributes(place_params)
     redirect_to root_path
   end
 
   def destroy
     @place = Place.find(params[:id])
+    if @place.user != current_user  
+      return render plain: 'Not Allowed', status: :forbidden
+    end
     @place.destroy
     redirect_to root_path
   end
