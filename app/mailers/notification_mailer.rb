fb7c7c837f97d5c7d1ae7ed0014bdc384982d484
Sean Cook <you@sean.bernard.cook@gmail.com>
2019-03-30 21:59:06 +0000

    couldnt email from dev environment, photos still not working

--- app/mailers/notification_mailer.rb
+++ app/mailers/notification_mailer.rb
@@ -1,8 +1,8 @@
 class NotificationMailer < ApplicationMailer
-    default from: "no-reply@nomsterapp.com"
-  
-    def comment_added
-      mail(to: "sean.bernard.cook@gmail.com",
-           subject: "A comment has been added to your place")
-    end
+  default from: "no-reply@nomsterapp.com"
+
+  def comment_added
+    mail(to: "sean.bernard.cook@gmail.com",
+         subject: "A comment has been added to your place")
   end
+end
