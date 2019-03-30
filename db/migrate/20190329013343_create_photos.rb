c8f847933b7eb63636df843bf63c8d3fae085f3d
Sean Cook <you@sean.bernard.cook@gmail.com>
2019-03-30 17:53:45 +0000

    fixed photos db

--- db/migrate/20190329013343_create_photos.rb
+++ db/migrate/20190329013343_create_photos.rb
@@ -1,9 +1,13 @@
 class CreatePhotos < ActiveRecord::Migration[5.2]
   def change
     create_table :photos do |t|
-      t.string :caption
-      t.string :place_id
+      t.text :caption
+      t.integer :user_id
+      t.integer :place_id
       t.timestamps
     end
+
+    add_index :photos, [:user_id, :place_id]
+    add_index :photos, :place_id
   end
 end
