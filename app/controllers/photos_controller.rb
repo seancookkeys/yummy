75335603c8112c0d0ee6a903774b3491e0bab9f1
Sean Cook <you@sean.bernard.cook@gmail.com>
2019-03-31 21:31:52 +0000

    troubleshooted on photos controlller with no luck

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -1,15 +1,15 @@
 class PhotosController < ApplicationController
-    #before_action :authenticate_user!
+    before_action :authenticate_user!
 
     def create
         @place = Place.find(params[:place_id])
-        @place.photos.create(photo_params)
+        @place.photos.create(photo_params.merge(user: current_user))
         redirect_to place_path(@place)
     end 
 
     private
 
     def photo_params
-        params.require(:photo).permit(:caption, :caption)
+        params.require(:photo).permit(:caption)
     end
 end
--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,4 +1,4 @@
 class Photo < ApplicationRecord
-    belongs_to :place
     belongs_to :user
-end
+    belongs_to :place
+  end
