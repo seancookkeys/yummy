9feb363482f6d6492a9e8ff172f2b64183b6b778
Sean Cook <you@sean.bernard.cook@gmail.com>
2019-04-17 01:13:18 +0000

    what you changed

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,12 +1,13 @@
 # frozen_string_literal: true
-
-CarrierWave.configure do |config|
-  config.fog_provider = 'fog/aws'
-  config.fog_credentials = {
-    provider: 'AWS',
-    aws_access_key_id: ENV['AWS_ACCESS_KEY'],
-    aws_secret_access_key: ENV['AWS_SECRET_KEY'],
-    region: ENV['AWS_S3_REGION']
-  }
-  config.fog_directory = ENV['AWS_S3_BUCKET']
+if Rails.env.production? 
+  CarrierWave.configure do |config|
+    config.fog_provider = 'fog/aws'
+    config.fog_credentials = {
+      provider: 'AWS',
+      aws_access_key_id: ENV['AWS_ACCESS_KEY'],
+      aws_secret_access_key: ENV['AWS_SECRET_KEY'],
+      region: ENV['AWS_S3_REGION']
+    }
+    config.fog_directory = ENV['AWS_S3_BUCKET']
+  end
 end
